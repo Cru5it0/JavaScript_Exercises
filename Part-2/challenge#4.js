e9422018472f92c72c122e4ed1603f460fb5c012
Cru5it0 <cruzisaacarandaervantes@gmail.com>
2023-09-14 11:07:44 -0600

    challenge 4

---
+++ Part-2/challenge#4.js
@@ -0,0 +1,21 @@
+const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+  }
+  
+  /* Write your code below. Good luck! 🙂 */
+  
+  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+  //console.log(calcTip(bills[0]));
+  
+  const tips = [];
+  const totals = [];
+  
+  for(let i = 0; i < bills.length; i++){
+      
+      const tip = calcTip(bills[i]);
+      tips.push(tip);
+      totals.push(tip + bills[i]);
+  
+  }
+  
+  console.log(bills, tips, totals);
