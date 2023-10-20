a82ecb681ded770223d654827ef50e912ab01399
Isaac Cervantes(UST <233566@ust-global.com>
2023-10-20 12:17:38 -0600

    Update of script, highscore correction

--- Guess_My_Number/script.js
+++ Guess_My_Number/script.js
@@ -89,7 +89,6 @@ document.querySelector('.again').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#222';
     document.querySelector('.number').style.width = '15rem';
-    document.querySelector('.highscore').textContent = 0;
 });
 
 
